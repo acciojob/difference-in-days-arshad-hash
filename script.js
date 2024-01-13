96cc8a4727964b5ec7185a7c1753169eab52fef6
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2024-01-13 18:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var dateDiffInDays = function (date1, date2) {
+	let one =new Date (date1);
+	let two= new Date (date2);  
+
+return Math.floor(((two-one)/1000)/86400) ;
+	
+	
   //   write your code here
 };
 
